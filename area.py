3e34386c38033dbcd5b1121b331bf0fb6063d551
Niharika Gupta <xaidmetamorphos@gmail.com>
2023-01-31 12:25:03 +0530

    OPERATORS, INPUT

---
+++ area.py
@@ -0,0 +1,5 @@
+# input('prompt')
+r = int(input(" Enter radius of circle:😁"))
+area = 22/7 * r **2
+print(f'Radius of cirlce {r}')
+print(f'Area of circle is {area}')
---
+++ info.py
@@ -0,0 +1,26 @@
+name = input("Enter ur name:")
+city = input("Enter ur city:")
+fav_actor = input("Enter ur fav actor:")
+fav_color = input("Enter ur fav color:")
+fav_place = input("Enter ur fav place:")
+fav_actrees =input("Enter ur fav actress:")
+fav_movie = input("Enter ur fav movie:")
+fav_song= input("Enter ur fav song:")
+fav_dog= input("Enter ur fav dog:")
+fav_player= input("Enter ur fav player:")
+
+print("!!!!Info!!!!")
+print(f'{name=}')
+print(f'{city=}')
+print(f'{fav_actor=}')
+print(f'{fav_color=}')
+print(f'{fav_place=}')
+print(f'{fav_actrees=}')
+print(f'{fav_movie=}')
+print(f'{fav_song=}')
+print(f'{fav_dog=}')
+print(f'{fav_player=}')
+
+
+
+
